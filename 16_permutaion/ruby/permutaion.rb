470f59cf9e5dcc0b8b5ae3d69cff267c4f4405eb
DAI ANDO <daifloss@gmail.com>
2017-07-13 07:29:31 +0900

    amicable

---
+++ 16_permutaion/ruby/permutaion.rb
@@ -0,0 +1,24 @@
+# p [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].permutation.to_a[999_999].join
+
+ # arr = [1,2,3]
+p [0, 1, 2, 3].permutation.to_a
+ p [0, 1, 2, 3].permutation.to_a[1_1]
+ p [0, 1, 2, 3].permutation.to_a[0_1]
+ p [0, 1, 2, 3].permutation.to_a[1_2]
+
+ arr = [
+   [23, 34, 45],
+   [24, 35, 46],
+   [25, 36, 47],
+   [26, 37, 48],
+ ]
+
+p arr[1_1]
+p arr[0_1]
+p arr[0_0]
+arr[1_1] = [2,3,4]
+p arr
+arr[1_0] = [22,33,44]
+p arr
+
+p 12_3
